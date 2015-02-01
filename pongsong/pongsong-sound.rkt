4f7f8bae40fbe69ccbe004d06a3b13ab2b5fff5e
darrencruse <darren.cruse@gmail.com>
2015-02-01 14:12:48 -0600

    switch to mp3s (IE doesn't support wav files)

--- pongsong/pongsong-sound.rkt
+++ pongsong/pongsong-sound.rkt
@@ -8,12 +8,13 @@
 
 ;; Make a sound (and return true)
 ;; what is one of "paddle", "wall", "missed", or "none"
+;; note: IE doesn't like .wav's but .mp3s are supported everywhere
 (define (play-pong-sound what)
   (begin
     (cond
-      [(string=? what "paddle") (play-sound "sounds/bloop3.wav")]
-      [(string=? what "wall") (play-sound "sounds/bloop2.wav")]
-      [(string=? what "missed") (play-sound "sounds/missed1.wav")]
+      [(string=? what "paddle") (play-sound "sounds/bloop3.mp3")]
+      [(string=? what "wall") (play-sound "sounds/bloop2.mp3")]
+      [(string=? what "missed") (play-sound "sounds/missed1.mp3")]
       [else true] ;; we remain silent if it's not a sound we recognize
     )
     true))
